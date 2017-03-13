d71bc34dd70408ceb9e0f48953c9fb44c63b12ed
jzao <joao.zao@blip.pt>
2017-03-13 11:43:51 +0000

    Added base application class

---
+++ app/src/main/java/com/tryus/snaporguess/BaseApplication.java
@@ -0,0 +1,16 @@
+package com.tryus.snaporguess;
+
+import android.app.Application;
+
+/**
+ * Created by joaozao on 13/03/17.
+ * This class contains the application initializations.
+ */
+
+public class BaseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
